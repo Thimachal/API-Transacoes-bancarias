5c0d3ffdafa15a45a0074e06eeb43d5fe1afe887
Thiago Oliveira <thimachal@gmail.com>
2022-08-01 15:24:27 -0300

    endpoint_detalhar_transacao

--- src/controllers/transactions.js
+++ src/controllers/transactions.js
@@ -13,4 +13,24 @@ const listTransactions = async (req, res) => {
     }
 }
 
-module.exports = {listTransactions};
+const detailTransaction = async (req, res) => {
+    const {user} = req;
+    const {id} = req.params;
+
+    try {
+        const {rowCount, rows} = await query('select * from transacoes where usuario_id = $1 and id = $2', [user.id, id]);
+
+        if(rowCount <= 0){
+            return res.status(404).json({mensagem: 'Transação não encontrada'});
+        }
+
+        const [transaction] = rows;
+        return res.json(transaction);
+
+    } catch (error) {
+        return res.status(500).json({mensagem: `Erro interno: ${error.message}`});
+    }
+
+}
+
+module.exports = {listTransactions, detailTransaction};
--- src/routes.js
+++ src/routes.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const {cadastroUser, getUser, updateUser} = require('./controllers/users');
 const {listCategories} = require('./controllers/categories');
-const {listTransactions} = require('./controllers/transactions');
+const {listTransactions,detailTransaction} = require('./controllers/transactions');
 const {login} = require('./controllers/login');
 const {filter} = require('./intermediary/authentication');
 
-
 const routes = express();
+
 //rotas de cadastro de usuário e login
 routes.post('/user', cadastroUser);
 routes.post('/login', login);
 
 //middleware para autenticação
 routes.use(filter);
+
 //rotas de usuário
 routes.get('/user', getUser);
 routes.put('/user', updateUser);
+
 //rotas de categorias
 routes.get('/categories', listCategories);
+
 //rotas de transações
 routes.get('/transactions', listTransactions);
+routes.get('/transactions/:id', detailTransaction);
 
 module.exports = routes;
